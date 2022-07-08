871e7c5a73ee4d17cb6886a728dc6416004cb383
onefootfarina <clichecontent@gmail.com>
2022-07-08 13:50:21 -0400

    added some sort of cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react';
+import Card from './components/Card';
 import './App.css';
 
 function App() {
+    const [cards, changeCards] = useState([
+        {
+            id: 1,
+            front: 'hello this is some text',
+            back: 'this is text for the back, this is text for the back, this is text for the back, this is text for the back , this is text for the back , ',
+        },
+        {
+            id: 2,
+            front: 'more text seconds card',
+            back: 'back 2nds card',
+        },
+    ]);
+
+    function addNewCard() {
+        const front = prompt('front card');
+        const back = prompt('back card');
+        changeCards((old) => [...old, { front: front, back: back }]);
+    }
+
     return (
         <div className="App">
-            <h1>hi</h1>
+            {cards.map((card) => {
+                return <Card frontText={card.front} backText={card.back} />;
+            })}
+            <button onClick={addNewCard}>click</button>
         </div>
     );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+function Card({ frontText, backText }) {
+    const [frontStyle, changeFront] = useState({
+        display: 'flex',
+    });
+    const [backStyle, changeBack] = useState({
+        display: 'none',
+    });
+    const [frontIsOpen, changeOpen] = useState(true);
+
+    function changeView() {
+        if (frontIsOpen) {
+            changeFront({ display: 'flex' });
+            changeBack({ display: 'none' });
+            changeOpen(false);
+        } else {
+            changeFront({ display: 'none' });
+            changeBack({ display: 'flex' });
+            changeOpen(true);
+        }
+    }
+
+    return (
+        <>
+            <div onClick={changeView} className="flashcard">
+                <h2 className="flashcardHeader">Header</h2>
+                <p className="flashcardText frontText" style={frontStyle}>
+                    {frontText}
+                </p>
+                <p className="flashcardText backText" style={backStyle}>
+                    {backText}
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default Card;
